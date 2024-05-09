83b3989d89fa805877d168c0ec4f05aa29dfda81
AbdullahIrfan <106930504+Abdullahirfansheikh@users.noreply.github.com>
2024-05-10 04:56:39 +0500

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,17 +1,19 @@
 "use client"
 import 'tailwindcss/tailwind.css';
+import React, { useEffect } from "react";
 import { usePathname } from "next/navigation";
-import React from 'react';
-
-
+import { Inter } from "next/font/google";
+const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({ children }) {
   const pathname = usePathname();
   return (
-    <html lang="en">
-      <body>         
-        {children}
-      </body>
-    </html>
+    
+      <html lang="en">
+        <body>         
+          {children}
+        </body>
+      </html>
+    
   );
 }
